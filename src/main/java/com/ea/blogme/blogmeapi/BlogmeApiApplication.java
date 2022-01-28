4d634101de9a749db8aa322bc19067394a568f85
Kapil Dangol <kadangol@miu.edu>
2022-01-28 15:15:34 -0600

    initial commit

---
+++ src/main/java/com/ea/blogme/blogmeapi/BlogmeApiApplication.java
@@ -0,0 +1,13 @@
+package com.ea.blogme.blogmeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogmeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogmeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ea/blogme/blogmeapi/BlogmeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ea.blogme.blogmeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogmeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
